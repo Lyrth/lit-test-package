ff9e7a47278f27e1560d827fdf1ca54f9b70fa0f
Lyrthras <defelixanne@gmail.com>
2022-05-03 19:21:50 +0800

    bump; clean workflow steps

--- package.lua
+++ package.lua
@@ -1,6 +1,6 @@
 return {
   name = "Lyrth/test-package",
-  version = "0.0.3",
+  version = "0.0.4",
   description = "Test package",
   tags = { "lua", "lit", "luvit" },
   license = "Apache License 2.0",
